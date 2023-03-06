d7b926cf5a0d53898cecadb6e2b185ad2672d77a
Ayi Akbar Maulana <ayiakbar66@gmail.com>
2023-03-06 17:48:14 +0700

    second init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm' : '300px',
+      'lg' : '1000px',
+      'xl' : '1280px'
+    },
     colors: {
       'almost-white' : 'hsl(0, 0%, 98%)',
       'medium-gray': 'hsl(0, 0%, 41%)',
