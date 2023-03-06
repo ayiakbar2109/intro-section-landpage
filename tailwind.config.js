aefcfca8ff6fd52d089bcaa195c9d32b24659847
ayiakbar2109 <ayiakbarmaulana21@gmail.com>
2023-03-06 10:05:04 +0700

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'almost-white' : 'hsl(0, 0%, 98%)',
+      'medium-gray': 'hsl(0, 0%, 41%)',
+      'almost-black': 'hsl(0, 0%, 8%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
